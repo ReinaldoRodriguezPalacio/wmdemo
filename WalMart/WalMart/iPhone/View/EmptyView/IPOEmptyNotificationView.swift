201a2f4b364d1810e5774fd5867e8e92abb9dd58
alejandro miranda aguilar <alejandro.miranda@bcgdvlabs.com>
2015-09-24 17:28:56 -0500

    etiqueta de notificaciones !

--- WalMart/WalMart/iPhone/View/EmptyView/IPOEmptyNotificationView.swift
+++ WalMart/WalMart/iPhone/View/EmptyView/IPOEmptyNotificationView.swift
@@ -14,12 +14,21 @@ class IPOEmptyNotificationView : IPOEmptyView {
     override func setup() {
         super.setup()
         
+        self.descLabel.text =  NSLocalizedString("notificationEmptyscreen.title",comment:"")
+        
         iconImageView.image = UIImage(named:"notification_emptyscreen")
         self.returnButton.alpha = 0
+        
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone  {
+            descLabel.frame = CGRectMake(0.0, 28.0, self.bounds.width, 16.0)
+        } else {
+            descLabel.frame = CGRectMake(0.0, 56.0, self.bounds.width, 16.0)
+        }
     }
     
     
