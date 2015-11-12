9eb5e2166607ea8851b37c450ba2ba221bf2bd98
Alonso Salcido <luis.salcido@bcgdvlabs.com>
2015-11-11 21:04:45 -0600

    Correxion después del merge

--- WalMart/WalMart/Shared/View/Banner/BannerCollectionViewCell.swift
+++ WalMart/WalMart/Shared/View/Banner/BannerCollectionViewCell.swift
@@ -96,6 +96,7 @@ class BannerCollectionViewCell : UICollectionViewCell, UIPageViewControllerDataS
             }
         }else {
             self.currentItem = dataSource!.count - 1
+            addCurrent = true
         }
         backAction = true
         return getCurrentController()
@@ -111,13 +112,13 @@ class BannerCollectionViewCell : UICollectionViewCell, UIPageViewControllerDataS
                 self.currentItem = 0
             }
             addCurrent = false
+            backAction = false
            return getCurrentController()
     }
     
     func getCurrentController() -> HomeBannerImageViewController {
         
         if dataSource?.count > 0 {
-        
             var bannerUrl = ""
             let dictBanner = dataSource![self.currentItem!]
             if let strUrl = dictBanner["urlPhone"]  {
@@ -209,6 +210,9 @@ class BannerCollectionViewCell : UICollectionViewCell, UIPageViewControllerDataS
         }
         if currentItem!  == dataSource?.count {
             currentItem = 0
+        }
+        if currentItem!  > dataSource?.count {
+            currentItem = currentItem! - dataSource!.count
         }
         
         self.visibleItem = currentItem!
