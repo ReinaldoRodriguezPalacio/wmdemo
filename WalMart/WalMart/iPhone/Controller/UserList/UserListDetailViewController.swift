94e88cf8964f6dfc9a68b17ab62273ac6bc6be49
Joel Juarez <joel.juarez@isol.com.mx>
2015-11-25 18:11:31 -0600

    teclado bloquea en listas

--- WalMart/WalMart/iPhone/Controller/UserList/UserListDetailViewController.swift
+++ WalMart/WalMart/iPhone/Controller/UserList/UserListDetailViewController.swift
@@ -1194,7 +1194,7 @@ class UserListDetailViewController: UserListNavigationBaseViewController, UITabl
     func updateLustName() {
         if self.nameField?.text != self.titleLabel?.text {
             
-            
+            self.nameField?.resignFirstResponder()
             self.alertView = IPOWMAlertViewController.showAlert(UIImage(named:"list_alert"), imageDone: UIImage(named:"done"), imageError: UIImage(named:"list_alert_error"))
             self.alertView!.setMessage(NSLocalizedString("list.message.updatingListNames", comment:""))
             
