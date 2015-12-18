02265f93d1a44488581b29e6db4224693ea9505b
Joel Juarez <joel.juarez@isol.com.mx>
2015-12-17 19:57:38 -0600

    url terms

--- WalMart/WalMart/Shared/View/Banner/BannerTermsView.swift
+++ WalMart/WalMart/Shared/View/Banner/BannerTermsView.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 
-class BannerTermsView : UIView {
+class BannerTermsView : UIView,UIGestureRecognizerDelegate {
     
     var imageBlurView : UIImageView!
     var viewBg : UIView!
     var viewBlurContainer : UIView!
     var viewText : UITextView!
     var onClose : (() -> Void)!
+    var openURL : ((url:String) -> Void)!
     
     func setup(text:String) {
         
@@ -34,21 +35,73 @@ class BannerTermsView : UIView {
         
         
         viewText = UITextView()
-        viewText.text = text
+        //viewText.text = text
         viewText.frame = CGRectMake( 16, 40, self.bounds.width - 32,  self.bounds.height - 50)
         viewText.font = WMFont.fontMyriadProRegularOfSize(14)
         viewText.textColor = UIColor.whiteColor()
         viewText.backgroundColor = UIColor.clearColor()
         viewText.editable = false
         viewText.selectable = false
+        
+        let terms = text.stringByReplacingOccurrencesOfString("<", withString: "").stringByReplacingOccurrencesOfString(">", withString: "")
+        let myString = NSMutableAttributedString(string: terms)
+        let test  =  text as NSString
+        if test.rangeOfString("<").location < 999 {
+            // Set an attribute on part of the string
+           
+            let startTxt:NSRange  = test.rangeOfString("<")
+            var endTxt:NSRange  = test.rangeOfString(">")
+            endTxt.length = (endTxt.location - startTxt.location) + 1
+            endTxt.location =  startTxt.location
+            
+            var stringurl = test.substringWithRange(endTxt)
+            stringurl = stringurl.stringByReplacingOccurrencesOfString("<", withString: "").stringByReplacingOccurrencesOfString(">", withString: "")
+            
+            let myRange:NSRange  = test.rangeOfString(stringurl)
+            //test.stringByReplacingOccurrencesOfString("<\(stringurl)>", withString:stringurl)
+            
+            let myCustomAttribute = [ "URLTERMS": stringurl]
+            myString.addAttributes(myCustomAttribute, range: myRange)
+            myString.addAttribute(NSForegroundColorAttributeName, value: UIColor.whiteColor(), range:NSMakeRange(0,myString.length))
+        }
+
+        viewText.attributedText = myString
+       
+        // Add tap gesture recognizer to Text View
+        let tap = UITapGestureRecognizer(target: self, action: Selector("openTermsTap:"))
+        tap.delegate = self
+        viewText.addGestureRecognizer(tap)
+        
+        
         viewBg.addSubview(viewText)
-        
-        
+
         let closeButton = UIButton(frame: CGRectMake(0, 0, 44, 44))
         closeButton.setImage(UIImage(named:"close"), forState: UIControlState.Normal)
         closeButton.addTarget(self, action: "removeFromSuperview", forControlEvents: UIControlEvents.TouchUpInside)
         viewBg.addSubview(closeButton)
         BaseController.sendAnalytics(WMGAIUtils.CATEGORY_BANNER_TERMS.rawValue, action: WMGAIUtils.ACTION_VIEW_BANNER_TERMS.rawValue, label: "")
+    }
+    
+    func openTermsTap(sender:UITapGestureRecognizer){
+        
+        let myTextView = sender.view as! UITextView
+        let layoutManager = myTextView.layoutManager
+        var location = sender.locationInView(myTextView)
+        location.x -= myTextView.textContainerInset.left;
+        location.y -= myTextView.textContainerInset.top;
+        
+        let characterIndex = layoutManager.characterIndexForPoint(location, inTextContainer: myTextView.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        
+        if characterIndex < myTextView.textStorage.length {
+            
+            let attributeName = "URLTERMS"
+            let attributeValue = myTextView.attributedText.attribute(attributeName, atIndex: characterIndex, effectiveRange: nil) as? String
+            if let value = attributeValue {
+                openURL(url: value)
+            }
+            
+        }
+    
     }
     
     
--- WalMart/WalMart/iPhone/Controller/Product/SearchProductViewController.swift
+++ WalMart/WalMart/iPhone/Controller/Product/SearchProductViewController.swift
@@ -754,7 +754,7 @@ class SearchProductViewController: NavigationViewController, UICollectionViewDat
         }
         
         let service = GRProductBySearchService()//TODO Agregar rating al idSort
-        self.brandText = self.idSort != "" ? "" : self.brandText
+       // self.brandText = self.idSort != "" ? "" : self.brandText
         let params = service.buildParamsForSearch(text: self.textToSearch, family: self.idFamily, line: self.idLine, sort: self.idSort == "" ? "" : self.idSort , departament: self.idDepartment, start: startOffSet, maxResult: self.maxResult,brand:self.brandText)
         service.callService(params,
             successBlock: { (arrayProduct:NSArray?) -> Void in
