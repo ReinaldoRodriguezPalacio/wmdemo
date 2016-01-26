91b117b979e26c3fda07039b1b61ca4d01ff3bdb
Luis Alonso Salcido Martinez <luis.salcido@isol.com>
2016-01-26 16:45:56 -0600

    fix mantis

--- WalMart/WalMart/iPhone/View/ShoppingCart/ShoppingCartTotalsTableViewCell.swift
+++ WalMart/WalMart/iPhone/View/ShoppingCart/ShoppingCartTotalsTableViewCell.swift
@@ -45,36 +45,36 @@ class ShoppingCartTotalsTableViewCell : UITableViewCell {
         subtotalTitle.textColor = WMColor.shoppingCartShopTotalsTextColor
         subtotalTitle.font = WMFont.fontMyriadProSemiboldOfSize(12)
         subtotalTitle.textAlignment = .Right
-        subtotalTitle.frame = CGRectMake(156, 18, 91, 12)
+        subtotalTitle.frame = CGRectMake(146, 18, 101, 12)
         
         iva = UILabel()
         iva.text = NSLocalizedString("shoppingcart.iva",comment:"")
         iva.textColor = WMColor.shoppingCartShopTotalsTextColor
         iva.font = WMFont.fontMyriadProSemiboldOfSize(12)
         iva.textAlignment = .Right
-        iva.frame = CGRectMake(156, subtotalTitle.frame.maxY + 6, 91, 12)
+        iva.frame = CGRectMake(146, subtotalTitle.frame.maxY + 6, 101, 12)
         
         total = UILabel()
         total.text = NSLocalizedString("shoppingcart.total",comment:"")
         total.textColor = WMColor.shoppingCartShopTotalsTextColor
         total.font = WMFont.fontMyriadProSemiboldOfSize(12)
         total.textAlignment = .Right
-        total.frame = CGRectMake(156, iva.frame.maxY + 20, 91, 12)
+        total.frame = CGRectMake(146, iva.frame.maxY + 20, 101, 12)
         
         totalSavingTitle = UILabel()
         totalSavingTitle.text = NSLocalizedString("shoppingcart.saving",comment:"")
         totalSavingTitle.textColor =  WMColor.savingTextColor
         totalSavingTitle.font = WMFont.fontMyriadProSemiboldOfSize(12)
         totalSavingTitle.textAlignment = .Right
-        totalSavingTitle.frame = CGRectMake(156, iva.frame.maxY + 3, 91, 12)
+        totalSavingTitle.frame = CGRectMake(146, iva.frame.maxY + 3, 101, 12)
         
-        valueSubtotal = CurrencyCustomLabel(frame: CGRectMake(subtotalTitle.frame.maxX + 3, subtotalTitle.frame.minY, 50, 12))
+        valueSubtotal = CurrencyCustomLabel(frame: CGRectMake(subtotalTitle.frame.maxX + 8, subtotalTitle.frame.minY, 50, 12))
         valueSubtotal.textAlignment = .Right
-        valueIva = CurrencyCustomLabel(frame: CGRectMake(iva.frame.maxX + 3, iva.frame.minY, 50, 12))
+        valueIva = CurrencyCustomLabel(frame: CGRectMake(iva.frame.maxX + 8, iva.frame.minY, 50, 12))
         valueIva.textAlignment = .Right
-        valueTotal = CurrencyCustomLabel(frame: CGRectMake(total.frame.maxX + 3, total.frame.minY, 50, 12))
+        valueTotal = CurrencyCustomLabel(frame: CGRectMake(total.frame.maxX + 8, total.frame.minY, 50, 12))
         valueTotal.textAlignment = .Right
-        valueTotalSaving = CurrencyCustomLabel(frame: CGRectMake(iva.frame.maxX + 3, totalSavingTitle.frame.minY, 50, 12))
+        valueTotalSaving = CurrencyCustomLabel(frame: CGRectMake(iva.frame.maxX + 8, totalSavingTitle.frame.minY, 50, 12))
         valueTotalSaving.textAlignment = .Right
 
         
