ec9893a1bbdf4a0fdd93ce81000fe2607059ff15
Luis Alonso Salcido Martinez <luis.salcido@isol.com>
2016-02-10 16:53:56 -0600

    service

--- WalMart/WalMart/Shared/Services/Product/GRProductDetailService.swift
+++ WalMart/WalMart/Shared/Services/Product/GRProductDetailService.swift
@@ -18,6 +18,14 @@ class GRProductDetailService : GRBaseService {
     
     let JSON_PRODUCTDETAIL_RESULT = "responseObject"
     
+    func buildParams(upc:String,collection:String) -> [String:AnyObject]{
+        if useSignalsServices {
+            let channel = IS_IPAD ? "ipad" : "iphone"
+            return ["upc":upc,"params":["eventtype": "pdpview","collection":"mg/dah","channel": channel]]
+        }
+        return ["upc":upc]
+    }
+    
     func callService(UPC:String,successBlock:((NSDictionary) -> Void)?, errorBlock:((NSError) -> Void)?) {
         self.callService(requestParams:UPC,successBlock: successBlock, errorBlock: errorBlock)
     }
