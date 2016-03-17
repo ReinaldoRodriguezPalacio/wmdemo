8f5c5a6264a0747b0c062715481a1596786c86ef
Joel Juarez <joel.juarez@isol.com.mx>
2016-03-16 18:11:20 -0600

    correcciones al zoom

--- WalMart/WalMart/Shared/View/Product/ProductDetailBannerCollectionViewCell.swift
+++ WalMart/WalMart/Shared/View/Product/ProductDetailBannerCollectionViewCell.swift
@@ -207,26 +207,27 @@ class ProductDetailBannerCollectionViewCell : UICollectionReusableView, UICollec
             widthNew = self.bounds.width
         }
         
-        if self.currentItem! > 0 {
+        
             if widthNew > 320 {
                 let heightNew = widthNew  - 320
                 self.collection.alpha = 0
                 
                 let cellImg = self.collection.cellForItemAtIndexPath(NSIndexPath(forItem: self.currentItem!, inSection: 0)) as? ProductDetailBannerMediaCollectionViewCell
-                let originRect = cellImg!.imageView!.frame
-                
-                let rectTransform = CGRectMake(originRect.minX - (heightNew / 2), originRect.minY, originRect.width + heightNew, originRect.height + heightNew)
-                
-                self.imageZoom.image = cellImg!.imageView.image
-                UIView.animateWithDuration(0, animations: { () -> Void in
-                    self.imageZoom.frame = rectTransform
-                    self.imageZoom.alpha = 1
-                })
+                if cellImg != nil {
+                    let originRect = cellImg!.imageView!.frame
+                    let rectTransform = CGRectMake(originRect.minX - (heightNew / 2), originRect.minY, originRect.width + heightNew, originRect.height + heightNew)
+                    
+                    self.imageZoom.image = cellImg!.imageView.image
+                    UIView.animateWithDuration(0, animations: { () -> Void in
+                        self.imageZoom.frame = rectTransform
+                        self.imageZoom.alpha = 1
+                    })
+                }
             } else {
                 self.collection.alpha = 1
                 self.imageZoom.alpha = 0
                 self.imageZoom.frame = collection.frame
-            }
+            
         }
         
         self.buildColorsAndSizesView()
