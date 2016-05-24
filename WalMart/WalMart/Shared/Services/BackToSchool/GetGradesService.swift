13c434c874149e1cbb9880c5e38cd09124882b0c
Luis Alonso Salcido Martinez <luis.salcido@isol.com>
2016-05-24 18:33:42 -0500

    Servicios BTS

---
+++ WalMart/WalMart/Shared/Services/BackToSchool/GetGradesService.swift
@@ -0,0 +1,23 @@
+//
+//  GetGradesService.swift
+//  WalMart
+//
+//  Created by Luis Alonso Salcido Martinez on 24/05/16.
+//  Copyright © 2016 BCG Inc. All rights reserved.
+//
+
+import UIKit
+
+class GetGradesService: BaseService {
+    
+    func callService(requestParams params:AnyObject, successBlock:((NSArray) -> Void)?, errorBlock:((NSError) -> Void)?) {
+        self.callPOSTService(params, successBlock: { (resultCall:NSDictionary) -> Void in
+            let arrayCall = resultCall["responseArray"] as! NSArray
+            successBlock!(arrayCall)
+        }) { (error:NSError) -> Void in
+            print("Error: \(error)")
+            errorBlock!(error)
+        }
+    }
+    
+}
