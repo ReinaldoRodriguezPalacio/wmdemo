497f8765fe8d0f71bd5c813b7f4e89212e47732d
Joel Juarez Alcantara <joel.juarez@isol.com.mx>
2016-08-30 17:22:57 -0500

    listas en carrito iPhone

--- WalMart/WalMart/iPhone/Controller/ShoppingCart/ShoppingCartViewController.swift
+++ WalMart/WalMart/iPhone/Controller/ShoppingCart/ShoppingCartViewController.swift
@@ -8,12 +8,13 @@
 
 import Foundation
 import QuartzCore
+import CoreData
 
 protocol ShoppingCartViewControllerDelegate {
     func closeShoppingCart()
 }
 
-class ShoppingCartViewController : BaseController ,UITableViewDelegate,UITableViewDataSource,ProductShoppingCartTableViewCellDelegate,SWTableViewCellDelegate,ProductDetailCrossSellViewDelegate,AlertPickerViewDelegate {
+class ShoppingCartViewController : BaseController ,UITableViewDelegate,UITableViewDataSource,ProductShoppingCartTableViewCellDelegate,SWTableViewCellDelegate,ProductDetailCrossSellViewDelegate,AlertPickerViewDelegate,ListSelectorDelegate {
     
     var viewLoad : WMLoadingView!
     
@@ -69,6 +70,8 @@ class ShoppingCartViewController : BaseController ,UITableViewDelegate,UITableVi
     var totalShop: Double = 0.0
     var selectQuantity: GRShoppingCartQuantitySelectorView?
     var facebookButton : UIButton!
+    
+    var listSelectorController: ListsSelectorViewController?
     
     
     override func getScreenGAIName() -> String {
@@ -330,67 +333,49 @@ class ShoppingCartViewController : BaseController ,UITableViewDelegate,UITableVi
         //self.navigationController!.popToRootViewControllerAnimated(true)
     }
 
-    /**
-     Add items from shopping cart to wishlist, call service Add Item Wishlist
-     */
+  
     func addToWishList () {
         
-        if !self.isWishListProcess {
-            self.isWishListProcess = true
-            let animation = UIImageView(frame: CGRectMake(0, 0,36, 36));
-            animation.center = self.buttonWishlist.center
-            animation.image = UIImage(named:"detail_addToList")
-            runSpinAnimationOnView(animation, duration: 100, rotations: 1, repeats: 100)
-            self.viewFooter.addSubview(animation)
-            var ixCount = 1
+        BaseController.sendAnalytics(WMGAIUtils.CATEGORY_SHOPPING_CART_SUPER.rawValue, categoryNoAuth: WMGAIUtils.CATEGORY_SHOPPING_CART_SUPER.rawValue, action: WMGAIUtils.ACTION_ADD_MY_LIST.rawValue, label: "")
+        
+        if self.listSelectorController == nil {
+            self.buttonWishlist!.selected = true
+            let frame = self.view.frame
+            self.listSelectorController = ListsSelectorViewController()
+            self.listSelectorController!.delegate = self
             
-            //EVENT
-            BaseController.sendAnalytics(WMGAIUtils.MG_CATEGORY_SHOPPING_CART_AUTH.rawValue,categoryNoAuth: WMGAIUtils.MG_CATEGORY_SHOPPING_CART_NO_AUTH.rawValue,action:WMGAIUtils.ACTION_ADD_ALL_WISHLIST.rawValue , label: "")
+            //self.listSelectorController!.productUpc = self.upc
+            self.addChildViewController(self.listSelectorController!)
+            self.listSelectorController!.view.frame = CGRectMake(0.0, frame.height, frame.width, frame.height)
+            self.view.insertSubview(self.listSelectorController!.view, belowSubview: self.viewFooter!)
+            self.listSelectorController!.titleLabel!.text = NSLocalizedString("gr.addtolist.super", comment: "")
+            self.listSelectorController!.didMoveToParentViewController(self)
+            self.listSelectorController!.view.clipsToBounds = true
             
-            for shoppingCartProduct  in self.itemsInShoppingCart {
-                let upc = shoppingCartProduct["upc"] as! String
-                let desc = shoppingCartProduct["description"] as! String
-                let price = shoppingCartProduct["price"] as! NSInteger
-                //let quantity = shoppingCartProduct["quantity"] as! String
-                
-                var onHandInventory = "0"
-                if let inventory = shoppingCartProduct["onHandInventory"] as? String {
-                    onHandInventory = inventory
+            self.listSelectorController!.generateBlurImage(self.view, frame: CGRectMake(0, 0, frame.width, frame.height))
+            self.listSelectorController!.imageBlurView!.frame = CGRectMake(0, -frame.height, frame.width, frame.height)
+            
+            UIView.animateWithDuration(0.5,
+                                       animations: { () -> Void in
+                                        self.listSelectorController!.view.frame = CGRectMake(0, 0, frame.width, frame.height)
+                                        self.listSelectorController!.imageBlurView!.frame = CGRectMake(0, 0, frame.width, frame.height)
+                },
+                                       completion: { (finished:Bool) -> Void in
+                                        if finished {
+                                            let footerFrame = self.viewFooter!.frame
+                                            self.listSelectorController!.tableView!.contentInset = UIEdgeInsetsMake(0, 0, footerFrame.height, 0)
+                                            self.listSelectorController!.tableView!.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, footerFrame.height, 0)
+                                        }
                 }
-                
-                let imageUrl = shoppingCartProduct["imageUrl"] as! String
-      
-                
-                var preorderable = "false"
-                if let preorder = shoppingCartProduct["isPreorderable"] as? String {
-                    preorderable = preorder
-                }
-
-                var category = ""
-                if let categoryVal = shoppingCartProduct["category"] as? String {
-                    category = categoryVal
-                }
-//Mustang sin wish list - agregar a listas
-//                
-//                let serviceAdd = AddItemWishlistService()
-//                if ixCount < self.itemsInShoppingCart.count {
-//                    serviceAdd.callService(upc, quantity: "1", comments: "", desc: desc, imageurl: imageUrl, price: "\(price)", isActive: "true", onHandInventory: onHandInventory, isPreorderable: preorderable,category:category, mustUpdateWishList: false, successBlock: { (result:NSDictionary) -> Void in
-//                        //let path = NSIndexPath(forRow: , inSection: 0)
-//
-//                        
-//                        }, errorBlock: { (error:NSError) -> Void in
-//                    })
-//                }else {
-//                    serviceAdd.callService(upc, quantity: "1", comments: "", desc: desc, imageurl: imageUrl, price: "\(price)", isActive: "true", onHandInventory: onHandInventory, isPreorderable: preorderable,category:category,mustUpdateWishList: true, successBlock: { (result:NSDictionary) -> Void in
-//                        self.showMessageWishList(NSLocalizedString("shoppingcart.wishlist.ready",comment:""))
-//                        animation.removeFromSuperview()
-//                        }, errorBlock: { (error:NSError) -> Void in
-//                            animation.removeFromSuperview()
-//                    })
-//                }
-                ixCount += 1
-                
-            }
+            )
+            
+            UIView.animateWithDuration(0.5, animations: { () -> Void in
+                self.listSelectorController!.view.frame = CGRectMake(0, 0, frame.width, frame.height)
+                self.listSelectorController!.imageBlurView!.frame = CGRectMake(0, 0, frame.width, frame.height)
+            })
+        }
+        else {
+            self.removeListSelector(action: nil)
         }
 
     }
@@ -1772,5 +1757,221 @@ class ShoppingCartViewController : BaseController ,UITableViewDelegate,UITableVi
 
     }
     
+    //MARK: ListSelectorDelegate
+    
+    func listSelectorDidShowList(listId: String, andName name:String) {
+        if let vc = storyboard!.instantiateViewControllerWithIdentifier("listDetailVC") as? UserListDetailViewController {
+            vc.listId = listId
+            vc.listName = name
+            vc.enableScrollUpdateByTabBar = false
+            self.navigationController!.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func listSelectorDidAddProduct(inList listId:String) {
+        self.alertView = IPOWMAlertViewController.showAlert(UIImage(named:"list_alert"), imageDone: UIImage(named:"done"),imageError: UIImage(named:"list_alert_error"))
+        self.alertView!.setMessage(NSLocalizedString("list.message.addingProductInCartToList", comment:""))
+        
+        let service = GRAddItemListService()
+        var products: [AnyObject] = []
+        for idx in 0 ..< self.itemsInShoppingCart.count {
+            let item = self.itemsInShoppingCart[idx] as! [String:AnyObject]
+            
+            let upc = item["upc"] as! String
+            var quantity: Int = 0
+            if  let qIntProd = item["quantity"] as? Int {
+                quantity = qIntProd
+            }
+            if  let qIntProd = item["quantity"] as? NSString {
+                quantity = qIntProd.integerValue
+            }
+            var pesable = "0"
+            if  let pesableP = item["type"] as? String {
+                pesable = pesableP
+            }
+            var active = true
+            if let stock = item["stock"] as? Bool {
+                active = stock
+            }
+            products.append(service.buildProductObject(upc: upc, quantity: quantity,pesable:pesable,active:active))
+        }
+        
+        service.callService(service.buildParams(idList: listId, upcs: products),
+                            successBlock: { (result:NSDictionary) -> Void in
+                                self.alertView!.setMessage(NSLocalizedString("list.message.addingProductInCartToListDone", comment:""))
+                                self.alertView!.showDoneIcon()
+                                self.alertView!.afterRemove = {
+                                    self.removeListSelector(action: nil)
+                                }
+            }, errorBlock: { (error:NSError) -> Void in
+                print("Error at add product to list: \(error.localizedDescription)")
+                self.alertView!.setMessage(error.localizedDescription)
+                self.alertView!.showErrorIcon("Ok")
+                self.alertView!.afterRemove = {
+                    self.removeListSelector(action: nil)
+                }
+            }
+        )
+    }
+    
+    func listSelectorDidAddProductLocally(inList list:List) {
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+        for idx in 0 ..< self.itemsInShoppingCart.count {
+            let item = self.itemsInShoppingCart[idx] as! [String:AnyObject]
+            
+            var quantity: Int = 0
+            if  let qIntProd = item["quantity"] as? Int {
+                quantity = qIntProd
+            }
+            if  let qIntProd = item["quantity"] as? NSString {
+                quantity = qIntProd.integerValue
+            }
+            
+            var price: Double = 0.0
+            if  let qIntProd = item["price"] as? NSNumber {
+                price = qIntProd.doubleValue
+            }
+            if  let qIntProd = item["price"] as? NSString {
+                price = qIntProd.doubleValue
+            }
+            
+            var typeProdVal: Int = 0
+            if let typeProd = item["type"] as? NSString {
+                typeProdVal = typeProd.integerValue
+            }
+            
+            
+            let detail = NSEntityDescription.insertNewObjectForEntityForName("Product", inManagedObjectContext: context) as? Product
+            detail!.upc = item["upc"] as! String
+            detail!.desc = item["description"] as! String
+            detail!.price = "\(price)"
+            detail!.quantity = NSNumber(integer: quantity)
+            detail!.type = NSNumber(integer: typeProdVal)
+            detail!.list = list
+            detail!.img = item["imageUrl"] as! String
+        }
+        
+        do {
+            try context.save()
+        } catch  {
+            print("Error save context listSelectorDidAddProductLocally")
+        }
+        
+        let count:Int = list.products.count
+        list.countItem = NSNumber(integer: count)
+        do {
+            try context.save()
+        } catch {
+            print("Error save context listSelectorDidAddProductLocally")
+        }
+        self.removeListSelector(action: nil)
+        
+    }
+    
+    func removeListSelector(action action:(()->Void)?) {
+        if self.listSelectorController != nil {
+            UIView.animateWithDuration(0.5,
+                                       delay: 0.0,
+                                       options: .LayoutSubviews,
+                                       animations: { () -> Void in
+                                        let frame = self.view.frame
+                                        self.listSelectorController!.view.frame = CGRectMake(0, frame.height, frame.width, 0.0)
+                                        self.listSelectorController!.imageBlurView!.frame = CGRectMake(0, -frame.height, frame.width, frame.height)
+                }, completion: { (complete:Bool) -> Void in
+                    if complete {
+                        if self.listSelectorController != nil {
+                            self.listSelectorController!.willMoveToParentViewController(nil)
+                            self.listSelectorController!.view.removeFromSuperview()
+                            self.listSelectorController!.removeFromParentViewController()
+                            self.listSelectorController = nil
+                        }
+                        self.buttonWishlist!.selected = false
+                        
+                        action?()
+                    }
+                }
+            )
+        }
+    }
+    
+    func listSelectorDidDeleteProductLocally(product:Product, inList list:List) {
+    }
+    
+    func listSelectorDidDeleteProduct(inList listId:String) {
+    }
+
+    func listSelectorDidShowListLocally(list: List) {
+        if let vc = storyboard!.instantiateViewControllerWithIdentifier("listDetailVC") as? UserListDetailViewController {
+            vc.listEntity = list
+            vc.listName = list.name
+            vc.enableScrollUpdateByTabBar = false
+            self.navigationController!.pushViewController(vc, animated: true)
+        }
+    }
+
+    
+    func listSelectorDidClose() {
+        self.removeListSelector(action: nil)
+    }
+    
+    func shouldDelegateListCreation() -> Bool {
+        return true
+    }
+    
+    func listSelectorDidCreateList(name:String) {
+        self.alertView = IPOWMAlertViewController.showAlert(UIImage(named:"list_alert"), imageDone: UIImage(named:"done"),imageError: UIImage(named:"list_alert_error"))
+        self.alertView!.setMessage(NSLocalizedString("list.message.addingProductInCartToList", comment:""))
+        
+        let service = GRSaveUserListService()
+        
+        var products: [AnyObject] = []
+        for idx in 0 ..< self.itemsInShoppingCart.count {
+            let item = self.itemsInShoppingCart[idx] as! [String:AnyObject]
+            
+            let upc = item["upc"] as! String
+            var quantity: Int = 0
+            if  let qIntProd = item["quantity"] as? NSNumber {
+                quantity = qIntProd.integerValue
+            }
+            else if  let qIntProd = item["quantity"] as? NSString {
+                quantity = qIntProd.integerValue
+            }
+            var price: String? = nil
+            if  let priceNum = item["price"] as? NSNumber {
+                price = "\(priceNum)"
+            }
+            else if  let priceTxt = item["price"] as? String {
+                price = priceTxt
+            }
+            
+            let imgUrl = item["imageUrl"] as? String
+            let description = item["description"] as? String
+            let type = item["type"] as? String
+            
+            var  nameLine = ""
+            if let line = item["line"] as? NSDictionary {
+                nameLine = line["name"] as! String
+            }
+            
+            let serviceItem = service.buildProductObject(upc: upc, quantity: quantity, image: imgUrl!, description: description!, price: price!, type: type,nameLine:nameLine)
+            products.append(serviceItem)
+        }
+        
+        service.callService(service.buildParams(name, items: products),
+                            successBlock: { (result:NSDictionary) -> Void in
+                                self.listSelectorController!.loadLocalList()
+                                self.alertView!.setMessage(NSLocalizedString("list.message.addingProductInCartToListDone", comment:""))
+                                self.alertView!.showDoneIcon()
+                                self.removeListSelector(action: nil)
+            },
+                            errorBlock: { (error:NSError) -> Void in
+                                print(error)
+                                self.alertView!.setMessage(error.localizedDescription)
+                                self.alertView!.showErrorIcon("Ok")
+            }
+        )
+    }
     
 }
