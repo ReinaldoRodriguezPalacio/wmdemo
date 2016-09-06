2e51aa6a58556abd066499215abb4f43b72a2b68
Daniel Vazquez <danivacruz@hotmail.com>
2016-09-05 19:32:53 -0500

    issue resuelto en Mis Pedidos

--- WalMart/WalMart/iPhone/View/EmptyView/IPOGenericEmptyView.swift
+++ WalMart/WalMart/iPhone/View/EmptyView/IPOGenericEmptyView.swift
@@ -29,12 +29,12 @@ class IPOGenericEmptyView : IPOEmptyView {
         
         descLabel.text = NSLocalizedString(IPOGenericEmptyViewSelected.Selected,comment:"")
         descLabel.numberOfLines = 3
-     
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.descLabel.frame = CGRectMake(50, 28.0, self.bounds.width - 100, 42)
+        self.returnButton.frame = CGRectMake((self.bounds.width - 160 ) / 2, self.bounds.size.height - 140, 160 , 40)
     }
     
     
