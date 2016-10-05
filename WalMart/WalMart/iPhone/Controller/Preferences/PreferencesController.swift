d151f46eda016c325ed7d7266796a1900a476628
Jesús Santa Olalla <jesus.santaolalla.picazo@gmail.com>
2016-10-05 13:28:20 -0500

    Se agrega vista de "Mis preferencias"

--- WalMart/WalMart/iPhone/Controller/Preferences/PreferencesController.swift
+++ WalMart/WalMart/iPhone/Controller/Preferences/PreferencesController.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
+enum PreferenceOptionsController : String {
+    
+    case Category = "Category"
+    case Notifications = "Notifications"
+    case ProductNotAvailable = "ProductNotAvailable"
+    case LegalInformation = "LegalInformation"
+    
+}
 
-
-class PreferencesController : NavigationViewController {
-
+class PreferencesController : NavigationViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var tablePreferences : UITableView!
+    
+    // "Categorías de mi interés", "Alertas y Notificaciones", "En caso de producto no disponible", "Información Legal"
+    
+    let preferenceOptions = [PreferenceOptionsController.Category.rawValue, PreferenceOptionsController.Notifications.rawValue, PreferenceOptionsController.ProductNotAvailable.rawValue, PreferenceOptionsController.LegalInformation.rawValue]
 
     override func getScreenGAIName() -> String {
         return WMGAIUtils.SCREEN_PREFERENCES.rawValue
@@ -19,7 +31,49 @@ class PreferencesController : NavigationViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.titleLabel!.text = NSLocalizedString("preferences.myPreferences", comment: "")
+        
+        tablePreferences = UITableView()
+        tablePreferences.dataSource = self
+        tablePreferences.delegate = self
+        
+        tablePreferences.registerClass(MoreMenuViewCell.self, forCellReuseIdentifier: "Cell")
+        tablePreferences.separatorStyle = UITableViewCellSeparatorStyle.None
+        
+        self.view.addSubview(tablePreferences)
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        self.tablePreferences.frame = CGRectMake(0, 46, self.view.bounds.width, self.view.bounds.height - 46)
+        
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return preferenceOptions.count
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 46.0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tablePreferences.dequeueReusableCellWithIdentifier("Cell") as! MoreMenuViewCell
+        let srtOption = preferenceOptions[indexPath.row]
+        
+        cell.isSeparatorComplete = true
+        cell.setPreferenceValues(srtOption, size: 16, colorText: WMColor.light_blue, colorSeparate: WMColor.light_gray)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+       
+    }
+    
 }
