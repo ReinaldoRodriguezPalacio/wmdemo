9062b0fb78698a661799000c3d851652928076dc
Luis Alonso Salcido Martinez <luis.salcido@isol.com>
2016-10-10 11:02:26 -0500

    fix errores

--- WalMart/WalMart/iPad/Controller/ShoppingCart/IPAShoppingCartViewController.swift
+++ WalMart/WalMart/iPad/Controller/ShoppingCart/IPAShoppingCartViewController.swift
@@ -255,7 +255,7 @@ class IPAShoppingCartViewController : ShoppingCartViewController, IPAGRCheckOutV
         productObje = listObj["products"] as! NSArray
         
         if section == (self.itemsInCartOrderSection.count) {
-            return productObje!.count + (self.itemsUPC.count > 0 ? 2 : 1)
+            return productObje!.count + (self.itemsUPC.count > 0 ? 1 : 0)
         } else {
             return productObje!.count
         }
