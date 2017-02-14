554e9b02acc60c95b1b06c9ceb900ca73d1a00bc
Joel Juarez Alcantara <joel.alcantara@bcgdvlabs.com>
2017-02-14 12:06:05 -0600

    merge

--- WalMart/WalMart/iPad/Controller/SearchProduct/IPASearchProductViewController.swift
+++ WalMart/WalMart/iPad/Controller/SearchProduct/IPASearchProductViewController.swift
@@ -374,7 +374,7 @@ class IPASearchProductViewController : SearchProductViewController, UIPopoverCon
                             pieces = (Int(quantity)!)
                         }
                         
-                        let params = self.buildParamsUpdateShoppingCart(cell, quantity: quantity, position: cell.positionSelected, orderByPiece: self.selectQuantityGR!.orderByPiece, pieces: pieces)
+                        let params = self.buildParamsUpdateShoppingCart(cell, quantity: quantity, position: cell.positionSelected, orderByPiece: true, pieces: pieces)
                         
                         ////BaseController.sendAnalytics(WMGAIUtils.MG_CATEGORY_SHOPPING_CART_AUTH.rawValue, categoryNoAuth:WMGAIUtils.MG_CATEGORY_SHOPPING_CART_NO_AUTH.rawValue , action: WMGAIUtils.ACTION_ADD_TO_SHOPPING_CART.rawValue, label:"\(cell.upc) - \(cell.desc)")
                         
